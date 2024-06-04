76de8c2956fa0437267d9a213a79f20fdec16983
linhnnase170113 <linhnnase170113@fpt.edu.vn>
2024-06-04 14:20:43 +0700

    Dashboard + ScanResults

--- src/app/head/_component/ScanResults.tsx
+++ src/app/head/_component/ScanResults.tsx
@@ -1,80 +1,154 @@
-import React from 'react'
-import { Button, Card, NavBar, Space, Toast } from 'antd-mobile'
-import { SearchOutline, MoreOutline, CloseOutline, AntOutline, RightOutline } from 'antd-mobile-icons'
+import React, { useState } from "react"
+import { Button, Card, Footer, List, NavBar, Space, Toast } from "antd-mobile"
+import { SearchOutline, MoreOutline, CloseOutline, AntOutline, RightOutline, AddOutline } from "antd-mobile-icons"
+import CreateIssueReport from "./CreateIssueReportFloatingPanel"
 
 const ScanResults = () => {
+   const [isPanelVisible, setIsPanelVisible] = useState(false)
 
+   const handleButtonClick = () => {
+      setIsPanelVisible(true)
+   }
+   const onClick = () => {
+      Toast.show("点击了卡片")
+   }
 
+   const onHeaderClick = () => {
+      Toast.show("点击了卡片Header区域")
+   }
 
-    const onClick = () => {
-        Toast.show('点击了卡片')
-    }
+   const onBodyClick = () => {
+      Toast.show("点击了卡片Body区域")
+   }
+   const right = (
+      <div style={{ fontSize: 24 }}>
+         <Space style={{ "--gap": "16px" }}>
+            <MoreOutline />
+         </Space>
+      </div>
+   )
 
-    const onHeaderClick = () => {
-        Toast.show('点击了卡片Header区域')
-    }
+   const back = () =>
+      Toast.show({
+         content: "点击了返回区域",
+         duration: 1000,
+      })
 
-    const onBodyClick = () => {
-        Toast.show('点击了卡片Body区域')
-    }
-    const right = (
-        <div style={{ fontSize: 24 }}>
-            <Space style={{ '--gap': '16px' }}>
-                <MoreOutline />
-            </Space>
-        </div>
-    )
-
-
-
-
-    const back = () =>
-        Toast.show({
-            content: '点击了返回区域',
-            duration: 1000,
-        })
-
-
-
-    return (
-        <div>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#FEF7FF', paddingTop: '0.5rem', paddingBottom: '0.5rem' }}>
-                <NavBar
-                    onBack={back}
-                    right={right}
-                    style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}
-                >
-                    Scan Results
-                </NavBar>
+   return (
+      <div style={{ maxHeight: '100vh', overflowY: 'auto' }}>
+         <div
+            style={{
+               display: "flex",
+               justifyContent: "center",
+               alignItems: "center",
+               backgroundColor: "#FEF7FF",
+               paddingTop: "0.5rem",
+               paddingBottom: "0.5rem",
+            }}
+         >
+            <NavBar
+               onBack={back}
+               right={right}
+               style={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%" }}
+            >
+               Scan Results
+            </NavBar>
+         </div>
+         <div style={{ marginTop: "3rem" }}>
+            <List>
+               <List.Item style={{ display: "block", paddingLeft: "0.5rem", paddingRight: "0.5rem" }}>
+                  <div style={{ display: "flex", gap: "8.2rem" }}>
+                     <p style={{ margin: 0, fontWeight: "bold" }}>Device ID</p>
+                     <p style={{ margin: 0 }}>AABBCC333</p>
+                  </div>
+               </List.Item>
+               <List.Item style={{ display: "block", paddingLeft: "0.5rem", paddingRight: "0.5rem" }}>
+                  <div style={{ display: "flex", gap: "10rem" }}>
+                     <p style={{ margin: 0, fontWeight: "bold" }}>Name</p>
+                     <p style={{ margin: 0 }}>Carbon Airaeter</p>
+                  </div>
+               </List.Item>
+               <List.Item style={{ display: "block", paddingLeft: "0.5rem", paddingRight: "0.5rem" }}>
+                  <div style={{ display: "flex", gap: "8.5rem" }}>
+                     <p style={{ margin: 0, fontWeight: "bold" }}>Category</p>
+                     <p style={{ margin: 0 }}>Manual Machines</p>
+                  </div>
+               </List.Item>
+               <List.Item style={{ display: "block", paddingLeft: "0.5rem", paddingRight: "0.5rem" }}>
+                  <div style={{ display: "flex", gap: "5rem" }}>
+                     <p style={{ margin: 0, fontWeight: "bold" }}>Production Date</p>
+                     <p style={{ margin: 0 }}>24/05/2010</p>
+                  </div>
+               </List.Item>
+               <List.Item style={{ display: "block", paddingLeft: "0.5rem", paddingRight: "0.5rem" }}>
+                  <div style={{ display: "flex", gap: "3.5rem" }}>
+                     <p style={{ margin: 0, fontWeight: "bold" }}>Last Servicing Date</p>
+                     <p style={{ margin: 0 }}>24/05/2024</p>
+                  </div>
+               </List.Item>
+               <List.Item style={{ display: "block", paddingLeft: "0.5rem", paddingRight: "0.5rem" }}>
+                  <div style={{ display: "flex", gap: "9rem" }}>
+                     <p style={{ margin: 0, fontWeight: "bold" }}>Position</p>
+                     <p style={{ margin: 0 }}>A001</p>
+                  </div>
+               </List.Item>
+               <List.Item style={{ display: "block", paddingLeft: "0.5rem", paddingRight: "0.5rem" }}>
+                  <div style={{}}>
+                     <p style={{ margin: 0, fontWeight: "bold" }}>Description</p>
+                     <p style={{ margin: 0 }}>
+                        This product has been in service for more than 14 years. Please be wary of performance issues
+                        throughout usage
+                     </p>
+                  </div>
+               </List.Item>
+            </List>
+         </div>
+         <div style={{ position: "relative", minHeight: "100vh" }}>
+            <div style={{ paddingBottom: "50px" /* Adjust based on the Footer height */ }}>
+               {/* Your main content here */}
             </div>
-            <div>
-                <Card
-                    title={
-                        <div style={{ fontWeight: 'normal' }}>
-                            <AntOutline style={{ marginRight: '4px', color: '#1677ff' }} />
-                            卡片标题
+            <Footer
+               label=""
+               content={
+                  <div style={{ display: "flex", gap: "3rem" }}>
+                     <Button
+                        style={{
+                           borderColor: "#454545",
+                           borderWidth: "0.5px",
+                           borderStyle: "solid",
+                           borderRadius: "5rem",
+                           color: "#6750A4",
+                        }}
+                     >
+                        <div style={{ display: "flex", gap: "0.3rem" }}>
+                           <p style={{ fontSize: "18px" }}>&#8592;</p>
+                           <p>Back</p>
                         </div>
-                    }
-                    extra={<RightOutline />}
-                    onBodyClick={onBodyClick}
-                    onHeaderClick={onHeaderClick}
-                    style={{ borderRadius: '16px' }}
-                >
-                    <div >卡片内容</div>
-                    <div onClick={e => e.stopPropagation()}>
+                     </Button>
+                     <div>
                         <Button
-                            color='primary'
-                            onClick={() => {
-                                Toast.show('点击了底部按钮')
-                            }}
+                           onClick={handleButtonClick}
+                           style={{ backgroundColor: "#6750A4", borderRadius: "5rem", color: "white", width: "18rem" }}
                         >
-                            底部按钮
+                           <div style={{ display: "flex", justifyContent: "center", gap: "0.5rem" }}>
+                              <p style={{ fontSize: "18px" }}>&#43;</p>
+                              <p>Make Report</p>
+                           </div>
                         </Button>
-                    </div>
-                </Card>
-            </div>
-        </div>
-    )
+                        <CreateIssueReport isVisible={isPanelVisible}/>
+                     </div>
+                  </div>
+               }
+               style={{
+                  position: "fixed",
+                  bottom: 0,
+                  width: "100%",
+                  backgroundColor: "white", // Optional: Adjust based on your Footer style
+               }}
+            />
+         </div>
+      </div>
+   )
 }
 
 export default ScanResults
--- src/app/head/page.tsx
+++ src/app/head/page.tsx
@@ -4,11 +4,13 @@ import { AntdRegistry } from "@ant-design/nextjs-registry";
 import React from "react";
 import ScanQR from "./_component/ScanQrScreen";
 import ScanResults from "./_component/ScanResults";
+import DashBoard from "./_component/DashBoard";
 
 export default function HeadHomePage({ children }: Readonly<{ children: React.ReactNode }>) {
    return (
       <AntdRegistry>
-         <ScanResults></ScanResults>
+         <DashBoard></DashBoard>
+         {/* <ScanResults></ScanResults> */}
       </AntdRegistry>
    )
 }

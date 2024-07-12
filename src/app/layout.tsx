90df86719619bd0ebf410cddc10fb155c8c91562
Sang-Dang <sangdase170287@fpt.edu.vn>
2024-07-12 11:19:38 +0700

    Working commit without multiple tasks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
-import type { Metadata } from "next"
+import type { Metadata, Viewport } from "next"
 import { ReactNode } from "react"
 import { Inter } from "next/font/google"
 import GlobalProvider from "@/common/providers/global.provider"
 import "@/app/globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
+export const viewport: Viewport = {
+   initialScale: 1.0,
+   width: "device-width",
+}
 
 export const metadata: Metadata = {
    title: "Rockstar App",

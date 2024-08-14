3f8ccd00b1adfad7d6d414f968a6766424e219e1
linhnnase170113 <linhnnase170113@fpt.edu.vn>
2024-08-15 05:52:42 +0700

    total

--- src/common/components/HomeHeader.tsx
+++ src/common/components/HomeHeader.tsx
@@ -2,11 +2,11 @@
 
 import { cn } from "@/common/util/cn.util"
 import { decodeJwt } from "@/common/util/decodeJwt.util"
-import { MoreOutlined } from "@ant-design/icons"
-import { Button, Col, Flex, Row, Skeleton, Typography } from "antd"
+import { Avatar, Button, Col, Flex, Row, Skeleton, Typography } from "antd"
 import Cookies from "js-cookie"
 import { CSSProperties, useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
+import { UserOutlined, MenuOutlined } from "@ant-design/icons"
 
 type HeadStaffDashboardHeaderProps = {
    className?: string
@@ -28,22 +28,29 @@ export default function HomeHeader(props: HeadStaffDashboardHeaderProps) {
 
    return (
       <Col className={cn(props.className)} style={props.style}>
-         <Row>
-            <Flex justify="flex-end" className="w-full">
-               <Button icon={<MoreOutlined />} type="default"></Button>
-            </Flex>
-         </Row>
-         <Row>
-            <Typography.Text className="text-base">Chào buổi sáng</Typography.Text>
-         </Row>
-         <Row>
-            {token ? (
-               <h1 className="mb-0 mt-1 text-3xl font-bold" key="name">
-                  {decodeJwt(token).username}
-               </h1>
-            ) : (
-               <Skeleton.Button className="h-12 w-full" key="load" />
-            )}
+         <Row gutter={[16, 0]} align="middle">
+            <Col>
+               <Flex justify="flex-start" className="w-full">
+                  <MenuOutlined style={{ color: '#FFFFFF', fontSize: '1.5rem' }} />
+               </Flex>
+            </Col>
+            <Col flex="auto">
+               <Row justify="end">
+                  <Typography.Text className="text-base text-white">Chào buổi sáng</Typography.Text>
+               </Row>
+               <Row justify="end">
+                  {token ? (
+                     <h1 className="mb-0 mt-1 text-3xl font-bold text-white" key="name">
+                        {decodeJwt(token).username}
+                     </h1>
+                  ) : (
+                     <Skeleton.Button className="h-12 w-full" key="load" />
+                  )}
+               </Row>
+            </Col>
+            <Col>
+               <Avatar style={{ backgroundColor: '#FFFFFF' }} size={60} icon={<UserOutlined style={{ color: '#6A7A91' }} />} />
+            </Col>
          </Row>
       </Col>
    )

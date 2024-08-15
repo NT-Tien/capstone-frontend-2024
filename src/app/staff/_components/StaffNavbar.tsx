46e26450d2245f26293105702e6cfd71e7ea2fa2
linhnnase170113 <linhnnase170113@fpt.edu.vn>
2024-08-15 13:25:24 +0700

    done1

--- src/app/staff/_components/StaffNavbar.tsx
+++ src/app/staff/_components/StaffNavbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import MobileNavbar, { NavbarMenuItem } from "@/common/components/MobileNavbar"
-import { CheckOutlined, ContainerFilled, DashboardFilled, EllipsisOutlined } from "@ant-design/icons"
+import { CheckOutlined, ContainerFilled, HomeFilled, SettingOutlined } from "@ant-design/icons"
 import { usePathname } from "next/navigation"
 import { useEffect, useState } from "react"
 
@@ -19,30 +19,30 @@ export default function StaffNavbar() {
 
    const items: NavbarMenuItem[] = [
       {
-         name: "Trang chủ",
+         name: "",
          key: "dashboard",
-         icon: <DashboardFilled />,
+         icon: <HomeFilled />,
          href: "/staff/dashboard",
          onClick: () => handleClick("dashboard"),
       },
       {
-         name: "Tác vụ",
+         name: "",
          key: "tasks",
          icon: <ContainerFilled />,
          href: "/staff/tasks",
          onClick: () => handleClick("tasks"),
       },
       {
-         name: "Bản đồ",
+         name: "",
          key: "map",
          icon: <CheckOutlined />,
          href: "/staff/map",
          onClick: () => handleClick("map"),
       },
       {
-         name: "Hồ sơ",
+         name: "",
          key: "profile",
-         icon: <EllipsisOutlined />,
+         icon: <SettingOutlined />,
          href: "/staff/profile",
          onClick: () => handleClick("profile"),
       },

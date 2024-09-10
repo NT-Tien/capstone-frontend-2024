d4761378304805306ba137bc1f3b0ed7b72046c1
linhnnase170113 <linhnnase170113@fpt.edu.vn>
2024-09-10 13:14:14 +0700

    admin-dashboard-ui-not-done

--- src/app/admin/_api/qk.ts
+++ src/app/admin/_api/qk.ts
@@ -3,5 +3,8 @@ import { Request as AllApiRequest } from "@/app/admin/_api/requests/all.api"
 export const admin_qk = {
     requests: {
         all: (request: AllApiRequest) => ["admin", "requests", "all", request],
+    },
+    devices: {
+        byAreaId: (id: string) => ["admin", "devices", "byAreaId", id]
     }
 }

37704a428b3bc2d1ced31e2a04b15186256d08a7
linhnnase170113 <linhnnase170113@fpt.edu.vn>
2024-09-22 17:49:12 +0700

    multiple options

--- src/app/head/(stack)/history/[id]/Feedback.drawer.tsx
+++ src/app/head/(stack)/history/[id]/Feedback.drawer.tsx
@@ -2,11 +2,11 @@ import { ReactNode, useState } from "react"
 import useModalControls from "@/common/hooks/useModalControls"
 import { App, Button, Card, Col, Drawer, Form, Input, Rate, Row } from "antd"
 import { InfoCircleFilled } from "@ant-design/icons"
-import { ProFormTextArea } from "@ant-design/pro-components"
+import { CheckCard, ProFormTextArea } from "@ant-design/pro-components"
 import { useMutation } from "@tanstack/react-query"
 import Head_Request_UpdateClose from "@/app/head/_api/request/update-close.api"
 import { UserOutlined } from "@ant-design/icons"
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation"
 import { FixRequestStatuses } from "@/common/dto/status/FixRequest.status"
 
 type Props = {
@@ -81,12 +81,41 @@ export default function FeedbackDrawer({
             </Card>
             <section className="mb-3 flex justify-center">
                <Col>
-                  <Row className="flex justify-center font-medium text-3xl">
+                  <Row className="flex text-3xl font-medium">
                      <UserOutlined />
+                     <Row className="m-1 font-medium">Đánh giá nhân viên</Row>
                   </Row>
-                  <Row className="font-medium">Chấm điểm cho nhân viên?</Row>
-                  <Row className="flex justify-center">
-                     <Rate />
+                  <Row gutter={[16, 16]} justify="center">
+                     <Col>
+                        <CheckCard
+                           style={{
+                              width: "10rem",
+                              height: "2.5rem",
+                              overflow: "hidden",
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                              textAlign: "center",
+                           }}
+                        >
+                           Thân thiện, lịch sự
+                        </CheckCard>
+                     </Col>
+                     <Col>
+                        <CheckCard
+                           style={{
+                              width: "10rem",
+                              height: "2.5rem",
+                              overflow: "hidden",
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                              textAlign: "center",
+                           }}
+                        >
+                           Hỗ trợ nhiệt tình
+                        </CheckCard>
+                     </Col>
                   </Row>
                </Col>
             </section>

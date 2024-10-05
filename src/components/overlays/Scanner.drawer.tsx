b302e94c8b42aa61adfbba433b12cc6b31d0ba90
linhnnase170113 <linhnnase170113@fpt.edu.vn>
2024-10-06 00:35:24 +0700

    1st

--- src/components/overlays/Scanner.drawer.tsx
+++ src/components/overlays/Scanner.drawer.tsx
@@ -1,17 +1,28 @@
 import ScannerInputManualDrawer from "@/components/overlays/ScannerInputManual.drawer"
 import useModalControls from "@/lib/hooks/useModalControls"
-import { InfoCircleOutlined, RightOutlined } from "@ant-design/icons"
+import { InfoCircleFilled, InfoCircleOutlined, RightOutlined } from "@ant-design/icons"
 import { Scanner } from "@yudiel/react-qr-scanner"
-import { Avatar, Button, Card, Drawer, DrawerProps } from "antd"
+import { App, Avatar, Button, Card, Drawer, DrawerProps, Form, Input } from "antd"
 import { ReactNode } from "react"
+import { isUUID } from "@/lib/utils/isUUID.util"
 
 type Props = {
    children: (handleOpen: () => void) => ReactNode
    onScan: (result: string) => void
    drawerProps?: DrawerProps
 }
-
-export default function ScannerDrawer({ children, ...props }: Props) {
+type FieldType = {
+   deviceId: string
+}
+export default function ScannerDrawer(
+   { children, ...props }: Props,
+   prop: {
+      children: (handleOpen: () => void) => ReactNode
+      onFinish: (deviceId: string, handleClose?: () => void) => Promise<void>
+      drawerProps?: DrawerProps
+      disabled?: boolean
+   },
+) {
    const { open, handleOpen, handleClose } = useModalControls({
       onClose: () => {},
    })
@@ -20,6 +31,26 @@ export default function ScannerDrawer({ children, ...props }: Props) {
       props.onScan(res)
       handleCloseManual?.()
       handleClose()
+   }
+   const [form] = Form.useForm<FieldType>()
+   const { message } = App.useApp()
+
+   async function handleFinishInput() {
+      try {
+         await form.validateFields()
+
+         const values = form.getFieldsValue()
+
+         await prop.onFinish(values.deviceId, handleClose)
+      } catch (e) {
+         message.destroy("error-msg")
+         message
+            .error({
+               content: "Không thể gửi dữ liệu. Vui lòng kiểm tra lại.",
+               key: "error-msg",
+            })
+            .then()
+      }
    }
 
    return (
@@ -30,7 +61,7 @@ export default function ScannerDrawer({ children, ...props }: Props) {
             onClose={handleClose}
             title="Quét mã QR"
             placement="bottom"
-            height="max-content"
+            height="max-height"
             destroyOnClose
             {...props.drawerProps}
          >
@@ -69,24 +100,65 @@ export default function ScannerDrawer({ children, ...props }: Props) {
                   },
                }}
             />
-            <ScannerInputManualDrawer onFinish={handleFinish}>
-               {(handleOpen) => (
-                  <Card size="small" hoverable onClick={handleOpen} className="mt-layout">
-                     <div className="flex items-center gap-3">
-                        <Avatar style={{ fontSize: "18px", textAlign: "center", backgroundColor: "#6750A4" }}>
-                           AI
-                        </Avatar>
-                        <div className="flex-grow">
-                           <p className="text-base font-bold">Nhập thủ công</p>
-                           <p className="text-xs">Nhấp vào đây nếu bạn không thể quét mã QR</p>
-                        </div>
-                        <div>
-                           <Button type="text" icon={<RightOutlined />} />
-                        </div>
+            <section className="p-4">
+               <Form<FieldType> disabled={prop.disabled} form={form} layout="horizontal">
+                  <Card size="small" hoverable className="mb-4">
+                     <div className="flex items-start gap-2">
+                        <InfoCircleFilled className="mt-1" />
+                        <div className="text-base">Vui lòng nhập ID của thiết bị nếu như bạn không thể quét mã QR</div>
                      </div>
                   </Card>
-               )}
-            </ScannerInputManualDrawer>
+                  <Form.Item<FieldType>
+                     name="deviceId"
+                     label={"ID của thiết bị"}
+                     labelAlign="left"
+                     labelCol={{
+                        span: 24,
+                        className: "pb-0",
+                     }}
+                     validateDebounce={1000}
+                     validateFirst
+                     rules={[
+                        { required: true },
+                        {
+                           validator: (_, value) =>
+                              isUUID(value) ? Promise.resolve() : Promise.reject("Invalid Device ID"),
+                        },
+                     ]}
+                     normalize={(original) => {
+                        let value = original.replace(/-/g, "")
+                        if (value.length > 8) {
+                           value = `${value.slice(0, 8)}-${value.slice(8)}`
+                        }
+                        if (value.length > 13) {
+                           value = `${value.slice(0, 13)}-${value.slice(13)}`
+                        }
+                        if (value.length > 18) {
+                           value = `${value.slice(0, 18)}-${value.slice(18)}`
+                        }
+                        if (value.length > 23) {
+                           value = `${value.slice(0, 23)}-${value.slice(23)}`
+                        }
+                        if (value.length > 36) {
+                           value = value.slice(0, 36)
+                        }
+                        return value
+                     }}
+                  >
+                     <Input
+                        placeholder="e.g., e31d662e-05db-4bc4-8bfd-773f56618725"
+                        size="large"
+                        autoComplete="off"
+                        aria-autocomplete="none"
+                        max={36}
+                        allowClear
+                     />
+                  </Form.Item>
+               </Form>
+               <Button key="submit-btn" type="primary" onClick={handleFinishInput} className="w-full" size="large">
+                  Gửi
+               </Button>
+            </section>
          </Drawer>
       </>
    )

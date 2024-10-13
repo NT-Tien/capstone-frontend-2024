2b413cb637fc02d7a8e65368cb958f14b8466ae0
linhnnase170113 <linhnnase170113@fpt.edu.vn>
2024-10-13 11:45:48 +0700

    1st

--- src/features/admin/api/request/dashboard.api.ts
+++ src/features/admin/api/request/dashboard.api.ts
@@ -6,6 +6,7 @@ import Cookies from "js-cookie"
 
 type Request = {
    type: "fix" | "warranty" | "renew" | "all"
+   areaId: string
    startDate: string
    endDate: string
 }
@@ -27,6 +28,7 @@ async function Admin_Requests_Dashboard(request: Request): Promise<Response> {
 Admin_Requests_Dashboard.URL = (req: Request) => {
    const urlparam = new URLSearchParams()
    urlparam.append("type", req.type)
+   urlparam.append("areaId", req.areaId)
    urlparam.append("startDate", req.startDate)
    urlparam.append("endDate", req.endDate)
 

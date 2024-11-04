3f6ba7443bc0cb0e3857aa2a0ca0c478b7dcfdf9
linhnnase170113 <linhnnase170113@fpt.edu.vn>
2024-11-05 03:14:05 +0700

    upsert done

--- src/features/admin/queries/Device_All.query.tsx
+++ src/features/admin/queries/Device_All.query.tsx
@@ -1,19 +1,23 @@
 import { useQuery, UseQueryOptions } from "@tanstack/react-query"
 import Admin_Devices_All, { Response } from "../api/device/all.api"
 
-type Props = Request
-type QueryOptions = UseQueryOptions<Response, Error, Response, (string | Props)[]>
+type Props = Request | undefined;
+type QueryOptions = UseQueryOptions<Response, Error, Response, (string | Props)[]>;
 
-useDevice_AllQuery.qk = (props: Props) => ["admin", "device", "all", props]
-useDevice_AllQuery.queryOptions = (props: Props): QueryOptions => ({
+useDevice_AllQuery.qk = (props?: Props) => {
+    return ["admin", "device", "all", props ? props : ""];
+};
+
+useDevice_AllQuery.queryOptions = (props?: Props): QueryOptions => ({
     queryKey: useDevice_AllQuery.qk(props),
     queryFn: () => Admin_Devices_All()
-})
-function useDevice_AllQuery(props: Props, queryOptions?: Omit<QueryOptions, "queryFn" | "queryKey">) {
+});
+
+function useDevice_AllQuery(props?: Props, queryOptions?: Omit<QueryOptions, "queryFn" | "queryKey">) {
     return useQuery({
         ...useDevice_AllQuery.queryOptions(props),
         ...queryOptions,
-    })
+    });
 }
 
-export default useDevice_AllQuery
+export default useDevice_AllQuery;
